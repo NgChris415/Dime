8867e7b278f979a68804d91adc2eb739e8a199f8
NgChris415 <132420552+NgChris415@users.noreply.github.com>
2023-09-12 21:37:29 -0700

    Updated name of App to align with website design

---
+++ dime/flask_app/controllers/users_controller.py
@@ -0,0 +1,58 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.users_model import Users
+from flask_app.models.sheets_model import Sheets
+from flask_app.models.expenses_model import Expenses
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+
+@app.route('/')
+def home():
+    return render_template('login_page.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not Users.validate_user(request.form):
+        return redirect('/')
+    hashed_pw = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email" : request.form['email'],
+        "password" : hashed_pw
+    }
+    user_id = Users.create_user(data)
+    session['user_id'] = user_id
+    return redirect('/dashboard')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = {
+        'email' : request.form['email'],
+        'password' :request.form['password']
+    }
+    existing_user = Users.get_user_by_email(data)
+    if not existing_user: 
+        flash('Invalid email/password')
+        return redirect('/')
+    if not bcrypt.check_password_hash(existing_user.password, request.form['password']):
+        flash('Invalid email/password')
+        return redirect('/')
+    session['user_id'] = existing_user.id
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template('dashboard.html', user = Users.get_user(data), sheets=Sheets.get_all_sheets())
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
---
+++ dime/flask_app/models/expenses_model.py
@@ -0,0 +1,55 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+
+db = 'Budget'
+
+class Expenses:
+    def __init__(self, data):
+        self.id = data['id']
+        # self.sheet_id = data['sheets_id']
+        self.location = data['location']
+        self.category = data['category']
+        self.amount_spent = data['amount_spent']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_expense(cls, data):
+        query = "SELECT * FROM expenses WHERE id = %(id)s;"
+        results = connectToMySQL(db).query_db(query, data)
+        print(results)
+        return cls(results[0])
+
+    @classmethod
+    def add_expense(cls, data):
+        query = 'INSERT INTO expenses (location, category, amount_spent, sheets_id) VALUES (%(location)s, %(category)s, %(amount_spent)s, %(sheet_id)s);'
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def delete_expense(cls,data):
+        query= 'DELETE FROM expenses WHERE id = %(id)s;'
+        return connectToMySQL(db).query_db(query,data)
+
+    @classmethod
+    def update_expense(cls,form_data):
+        query = "UPDATE expenses SET location = %(location)s, category = %(category)s, amount_spent = %(amount_spent)s WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query,form_data)
+
+    @classmethod
+    def get_all_expenses_by_id(cls, data):
+        query = 'SELECT * FROM expenses WHERE expenses.sheets_id = %(id)s;'
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @staticmethod
+    def validate_expense(form_data):
+        is_valid = True
+
+        if len(form_data['location']) < 2:
+            flash("Location must be at least 2 characters long")
+            is_valid = False
+        if int(form_data['amount_spent']) <= 0:
+            flash("Amount Spent must be greater than $0")
+            is_valid = False
+        return is_valid
